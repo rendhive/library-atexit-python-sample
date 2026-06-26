46cb7ee3e9c6a6f7799ccfaa77e62cc2438fcd1c
rendhive <rendhivecode@gmail.com>
2026-06-26 13:37:59 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul atexit.py

---
+++ 1. Mengimpor Modul atexit.py
@@ -0,0 +1,5 @@
+import atexit
+
+print("Modul atexit berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menetapkan fungsi untuk dieksekusi saat program selesai.
+# Kondisi: Digunakan sebelum mendefinisikan fungsi yang akan dieksekusi di akhir program.
